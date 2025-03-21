91e0c352d3512b624526f38446b07dff0423abd0
Damian674 <st_demian@hotmail.com>
2025-03-20 22:47:34 -0300

    carpeta programacion 1

---
+++ Programacion 1/Tp Secuenciales/TP Secuenciales - Damian Tristant.py
@@ -0,0 +1,71 @@
+#Ejercicio 1
+#print("Hola mundo!")
+
+#Ejercicio 2
+'''nombre = input("Ingrese su nombre: ")
+print(f"Hola {nombre}!")'''
+
+#Ejercicio 3
+'''nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+edad = int(input("Ingrese su edad: "))
+lugar_residencia = input("Ingrese su lugar de residencia: ")
+print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {lugar_residencia}")'''
+
+#Ejercicio 4
+'''radio = int(input("Ingrese el valor del radio para saber su area y perimetro: "))
+
+area = 3.14 * (radio * radio)
+perimetro = 3.14 * (radio * 2)
+
+print(f"El valor de area es: {area} y su perimetro: {perimetro}")'''
+
+#Ejercicio 5
+'''segundos = int(input("Ingrese una cantidad de segundos: "))
+hora = segundos / 3600
+
+print(f"Los segundos ingresados equivalen a {hora} horas")'''
+
+#Ejercicio 6
+
+'''numero = int(input("Introduce un número para ver su tabla de multiplicar: "))
+
+print(f"Tabla de multiplicar del {numero}:")
+for i in range(1, 11):
+print(f"{numero} x {i} = {numero * i}")'''
+
+#Ejercicio 7
+
+'''num1 = int(input("Ingrese el 1er numero que sea distinto de cero: "))
+num2 = int(input("Ingrese el 2do numero que sea distinto de cero: "))
+
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+
+print(f"La suma de ambos numeros es: {suma}, su resta: {resta}, su multiplicacion: {multiplicacion}, su division: {division} ")'''
+
+#Ejercicio 8
+'''altura = float(input("Ingrese su altura: "))
+peso = float(input("Ingrese su peso: "))
+
+imc = altura / (peso * peso)
+
+print(f"Su imc es: {imc}")'''
+
+#Ejercicio 9
+'''temp_celsius = float(input("Ingrese una temperatura en celsius: "))
+
+temp_fahrenheit = (1.8 * temp_celsius) + 32
+
+print(f"La temperatura en grados fahrenheit es: {temp_fahrenheit}")'''
+
+#Ejercicio 10
+'''num1 = int(input("Ingrese el numero 1: "))
+num2 = int(input("Ingrese el numero 2: "))
+num3 = int(input("Ingrese el numero 3: "))
+
+promedio = (num1 + num2 + num3) / 3
+
+print(f"El promedio de los numeros ingresados es: {promedio}")'''
