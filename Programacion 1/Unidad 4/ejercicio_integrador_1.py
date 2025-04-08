f5a7877f6a767e97c8812ce1000c940707007ff0
Damian674 <st_demian@hotmail.com>
2025-04-08 18:43:58 -0300

    Tp estruturas repetitivas

---
+++ Programacion 1/Unidad 4/ejercicio_integrador_1.py
@@ -0,0 +1,5 @@
+anio_nacimiento = int(input("Ingrese su año de nacimiento: "))
+anio_actual = 2025
+
+edad = anio_actual - anio_nacimiento
+print("Su edad es de",edad, "años")
---
+++ Programacion 1/Unidad 4/tp_estructuras_repetitivas.py
@@ -0,0 +1,7 @@
+'''Ejercicio 1
+
+#Imprimo los numeros del 0 a 100 por pantalla
+for i in range(101):
+    print(i)
+
+'''
