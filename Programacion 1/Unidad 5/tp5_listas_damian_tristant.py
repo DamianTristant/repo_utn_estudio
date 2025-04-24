b6775cdebb08f8bdb1263d63112b8053314e2f06
Damian674 <st_demian@hotmail.com>
2025-04-24 18:17:26 -0300

    Tp listas programacion 1

---
+++ Programacion 1/Unidad 5/tp5_listas_damian_tristant.py
@@ -0,0 +1,56 @@
+'''
+#Ejercicio 1
+
+multiplos_4 = list(range(4,101,4))
+print(multiplos_4)
+
+'''
+'''
+#Ejercicio 2
+
+mi_lista = [8.5,4,True,"casa","perro"]
+mi_lista_3 = mi_lista[3]
+print(mi_lista_3)
+
+'''
+'''
+#Ejercicio 3
+
+lista_vacia = []
+lista_vacia.append("Damian")
+lista_vacia.append("Rocio")
+lista_vacia.append("Gabriela")
+print(lista_vacia)
+
+'''
+'''
+#Ejercicio 4
+
+animales = ["perro", "gato", "conejo", "pez"]
+
+animales[1] = "loro"
+animales[3] = "oso"
+print(animales)
+
+'''
+'''
+
+#Ejercicio 5
+
+#Tengo una lista con numeros y con "Remove y max" elimino el numero mas grande, en este caso el 22
+
+'''
+
+'''
+#Ejercicio 6
+
+lista_numeros = []
+
+for numeros in range(10,31,5):
+    lista_numeros.append(numeros)
+
+print(lista_numeros[0:2])
+
+'''
+    
+
